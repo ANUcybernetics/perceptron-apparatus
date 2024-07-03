e3a08fb498048d4b069d1f4cf40db02387a9441c
Ben Swift <ben@benswift.me>
2024-07-03 12:15:18 +1000

    initial commit

---
+++ lib/perceptron_apparatus.ex
@@ -0,0 +1,18 @@
+defmodule PerceptronApparatus do
+  @moduledoc """
+  Documentation for `PerceptronApparatus`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PerceptronApparatus.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/perceptron_apparatus_test.exs
@@ -0,0 +1,8 @@
+defmodule PerceptronApparatusTest do
+  use ExUnit.Case
+  doctest PerceptronApparatus
+
+  test "greets the world" do
+    assert PerceptronApparatus.hello() == :world
+  end
+end
