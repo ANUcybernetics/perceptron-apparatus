7c0d05a0c7f63e188f8c95f62408b52c5412751e
Ben Swift <ben@benswift.me>
2024-07-04 13:39:59 +1000

    radial sliders: can draw the single slider track ok

--- lib/perceptron_apparatus/rings/radial_sliders.ex
+++ lib/perceptron_apparatus/rings/radial_sliders.ex
@@ -2,100 +2,110 @@ defmodule PerceptronApparatus.Rings.RadialSliders do
   @moduledoc """
   Documentation for `RadialSliders`.
   """
-  defstruct [:radial_size, :range, :shape, :layer_index]
+  defstruct [:width, :shape, :range, :context]
 
   @type t :: %__MODULE__{
           # min, max
           range: Range.t(),
-          # {groups, sliders_per_group}
-          shape: {integer(), integer()},
-          # radial size (often the default will be fine)
-          radial_size: float(),
-          # layer index, counted from outside-to-inside
-          layer_index: integer()
+          # this is not the geometric shape, rather the shape of the corresponding matrix
+          # {n_groups, n_sliders_per_group}
+          shape: {integer(), integer},
+          # ring width (r_outer - r_inner)
+          width: float(),
+          # drawing context: {r_outer, layer_index}
+          context: {float(), integer()}
         }
 
-  def new(opts \\ []) do
-    # shape is required
-    shape = Keyword.fetch!(opts, :shape)
+  def new(shape, opts \\ []) do
     # use default values when it makes sense
     range = Keyword.get(opts, :range, 0..10)
-    size = Keyword.get(opts, :radial_size, 100.0)
-    # layer index can be added later, nil ok at first
-    layer_index = Keyword.get(opts, :layer_index)
+    size = Keyword.get(opts, :width, 100.0)
 
-    %__MODULE__{radial_size: size, range: range, shape: shape, layer_index: layer_index}
+    %__MODULE__{width: size, range: range, shape: shape}
   end
 
-  def radial_slider(r_outer, length, theta) do
-    r_inner = r_outer - length
+  def put_context(ring, context) do
+    %{ring | context: context}
+  end
 
-    labels =
-      0..10
-      |> Enum.map(fn x ->
-        y = length - (2 + x * (length - 4) / 10.0)
+  defp labeller(x) do
+    cond do
+      Integer.mod(x, 5) == 0 -> Integer.to_string(x)
+      true -> ""
+    end
+  end
 
-        label =
-          cond do
-            Integer.mod(x, 5) == 0 -> Integer.to_string(x)
-            true -> ""
-          end
-
-        """
-          <line x1="-3"  y1="#{y}"  x2="3" y2="#{y}" stroke-width="0.3" />
-          <text x="0" y="#{y}"
-                style="font-size: 5px;" fill="black" stroke="none" stroke-width="0.3"
-                text-anchor="middle" dominant-baseline="middle"
-                >#{label}</text>
-        """
-      end)
-      |> Enum.join()
+  def render_slider(r_outer, width, theta) do
+    slider_hwidth = 5
 
     """
-    <g transform="rotate(#{theta}) translate(0 #{r_inner})"  transform-origin="0 0">
+    <g class="top full" transform="rotate(#{theta}) translate(0 #{r_outer})" transform-origin="0 0">
      <path
-      d="M -3 0
-        a 3 3 0 0 1 6 0
-        v #{length}
-        a 3 3 0 0 1 -6 0
-        v #{-length}"
-      stroke="red"
+      d="M -#{slider_hwidth} 0
+        a #{slider_hwidth} #{slider_hwidth} 0 0 0 #{2 * slider_hwidth} 0
+        v #{-width}
+        a #{slider_hwidth} #{slider_hwidth} 0 0 0 -#{2 * slider_hwidth} 0
+        v #{width}"
       />
-      #{labels}
       </g>
     """
   end
 
-  def radial_slider_group(r_outer, length, n_sliders, d_theta, offset_theta) do
-    0..(n_sliders - 1)
-    |> Enum.map(fn x -> radial_slider(r_outer, length, offset_theta + x * d_theta) end)
+  @doc """
+  - `theta` is the sweep angle of the slider group in degrees
+  - `theta_offset` is the angle offset of the slider group in degrees
+  - `range` is the range of values to be displayed
+  """
+  def render_group(
+        r_outer,
+        width,
+        sliders_per_group,
+        theta,
+        theta_offset,
+        range
+      ) do
+    _guide_lines =
+      range
+      |> Enum.map(fn val ->
+        range_min = Enum.min(range)
+        dynamic_range = Enum.max(range) - range_min
+        r = r_outer - width * (val - range_min) / dynamic_range
+
+        """
+          <circle class="top etch" cx="0" cy="0" r="#{r}" stroke-width="0.3" />
+          <text class="top etch" x="0" y="#{r}"
+                style="font-size: 5px;" fill="black" stroke="none" stroke-width="0.3"
+                text-anchor="middle" dominant-baseline="middle"
+                >#{labeller(val)}</text>
+        """
+      end)
+      |> Enum.join()
+
+    0..(sliders_per_group - 1)
+    |> Enum.map(fn i -> render_slider(r_outer, width, theta_offset + i * theta) end)
     |> Enum.join()
   end
 
-  def radial_slider_ring(r_outer, length, n_groups, sliders_per_group) do
-    d_theta =
-      case n_groups do
-        1 -> 360 / (sliders_per_group * n_groups)
-        _ -> 360 / ((sliders_per_group + 1) * n_groups)
-      end
+  # def render(%__MODULE__{} = ring, {r_outer, layer_index}) do
+  #   # r_outer, length, n_groups, sliders_per_group
+  #   %{shape: {n_groups, sliders_per_group}, range: range, width: length} = ring
 
-    0..(n_groups - 1)
-    |> Enum.map(fn x ->
-      radial_slider_group(r_outer, length, sliders_per_group, d_theta, 360 * x / n_groups)
-    end)
-    |> Enum.join()
-  end
+  #   d_theta =
+  #     case n_groups do
+  #       1 -> 360 / (sliders_per_group * n_groups)
+  #       _ -> 360 / ((sliders_per_group + 1) * n_groups)
+  #     end
+
+  #   0..(n_groups - 1)
+  #   |> Enum.map(fn x ->
+  #     group(r_outer, length, sliders_per_group, d_theta, 360 * x / n_groups)
+  #   end)
+  #   |> Enum.join()
+  # end
 end
 
 defimpl PerceptronApparatus.Renderable, for: PerceptronApparatus.Rings.RadialSliders do
-  def render(ring) do
-    %{position: {radius, length}, shape: {n_groups, sliders_per_group}} = ring
-
-    PerceptronApparatus.Rings.RadialSliders.radial_slider_ring(
-      radius,
-      length,
-      n_groups,
-      sliders_per_group
-    )
+  def render(_ring) do
+    "TODO"
   end
 end
