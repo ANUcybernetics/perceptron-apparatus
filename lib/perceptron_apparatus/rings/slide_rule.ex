b7b6ca1a3cc33c12be21c760bacc40b7455c039b
Ben Swift <ben@benswift.me>
2024-07-05 11:26:31 +1000

    SlideRule.relu_rule/2 generates the right data

--- lib/perceptron_apparatus/rings/slide_rule.ex
+++ lib/perceptron_apparatus/rings/slide_rule.ex
@@ -2,39 +2,43 @@ defmodule PerceptronApparatus.Rings.SlideRule do
   @moduledoc """
   Documentation for `SlideRule`.
   """
-  defstruct [:width, :outer_range, :inner_range, :context]
+  defstruct [:width, :rule, :context]
 
   @type t :: %__MODULE__{
-          # atom is scale type: :linear or :log
-          outer_range: {Range.t(), :atom},
-          inner_range: {Range.t(), :atom},
+          # rule consists of {outer_rule, inner_rule}
+          # each rule is a list of {value, theta} tuples
+          rule: {[{float(), float() | nil}], [{float(), float() | nil}]},
           # ring width (fixed for slide rules)
           width: float(),
           # drawing context: {outer_radius, layer_index}
           context: {float(), integer()}
         }
 
-  def new(outer_range, inner_range) do
-    %__MODULE__{width: 20.0, outer_range: outer_range, inner_range: inner_range}
+  def new(rule) do
+    %__MODULE__{width: 20.0, rule: rule}
   end
 
-  def render_slider(radius, outer_scale, inner_scale) do
+  # each rule should be a list of tuples {theta, label}, where label can be nil (for a minor tick with no label)
+  def render_slider(radius, {outer_rule, inner_rule}) do
     outer_ticks =
-      outer_scale
-      |> Enum.map(fn {pos, label} ->
+      outer_rule
+      |> Enum.map(fn {theta, val} ->
+        %{label: label, tick_length: tick_length, stroke_width: stroke_width} =
+          ticks_and_labels(val)
+
         """
-          <g transform="rotate(#{-pos})" transform-origin="0 0">
-            <text x="0" y="#{radius + 20}" style="font-size: 12px;" fill="black" stroke="none" text-anchor="middle" dominant-baseline="auto">#{label}</text>
-            <line x1="0" y1="#{radius}" x2="0" y2="#{if label != "", do: radius + 8, else: radius + 4}" />
+          <g transform="rotate(#{-theta})" transform-origin="0 0">
+            <text class="top etch" x="0" y="#{radius + 2 * tick_length}" style="font-size: 12px;" fill="black" stroke="none" text-anchor="middle" dominant-baseline="auto">#{label}</text>
+            <line class="top etch" x1="0" y1="#{radius}" x2="0" y2="#{radius + tick_length}" stroke_width="#{stroke_width}" />
           </g>
         """
       end)
 
     inner_ticks =
-      inner_scale
-      |> Enum.map(fn {pos, label} ->
+      inner_rule
+      |> Enum.map(fn {theta, label} ->
         """
-          <g transform="rotate(#{-pos})" transform-origin="0 0">
+          <g transform="rotate(#{-theta})" transform-origin="0 0">
             <text x="0" y="#{radius - 20}" style="font-size: 12px;" fill="black" stroke="none" text-anchor="middle" dominant-baseline="hanging">#{label}</text>
             <line x1="0" y1="#{if label != "", do: radius - 8, else: radius - 4}" x2="0" y2="#{radius}" />
           </g>
@@ -43,12 +47,12 @@ defmodule PerceptronApparatus.Rings.SlideRule do
 
     Enum.join([
       outer_ticks,
-      ~s|<circle class="top full" cx="0" ch="0" r="#{radius}" />|,
+      ~s|<circle class="top full" cx="0" cy="0" r="#{radius}" />|,
       inner_ticks
     ])
   end
 
-  def log_scale do
+  def log_rule do
     10..99
     |> Enum.map(fn x ->
       pos = (Math.log(x / 10.0) - Math.log(1.0)) / (Math.log(10.0) - Math.log(1.0)) * 360.0
@@ -62,31 +66,41 @@ defmodule PerceptronApparatus.Rings.SlideRule do
     end)
   end
 
-  def relu_outer do
-    -180..179//3
-    |> Enum.map(fn x ->
-      cond do
-        Integer.mod(x, 5) == 0 -> {x, Float.to_string(x / 30.0)}
-        true -> {x, ""}
-      end
-    end)
-  end
+  def relu_rule(max_value, delta_value) do
+    delta_theta = 180.0 * delta_value / max_value
 
-  def relu_inner do
-    -180..179//3
-    |> Enum.map(fn x ->
-      cond do
-        x <= 0 && Integer.mod(x, 5) == 0 -> {x, "0"}
-        Integer.mod(x, 5) == 0 -> {x, Float.to_string(x / 30.0)}
-        true -> {x, ""}
-      end
-    end)
+    outer_positive =
+      {0.0, 0.0}
+      |> Stream.iterate(fn {val, theta} -> {val + delta_value, theta + delta_theta} end)
+      |> Enum.take_while(fn {val, _theta} -> val <= max_value end)
+
+    outer_negative =
+      outer_positive
+      # remove first + last elements because that would overlap with the positive rule
+      |> List.delete_at(0)
+      |> List.delete_at(-1)
+      |> Enum.map(fn {val, theta} -> {-val, -theta} end)
+
+    outer_rule = Enum.reverse(outer_negative) ++ outer_positive
+
+    inner_rule =
+      outer_rule
+      |> Enum.map(fn
+        {val, theta} when val < 0 -> {0.0, theta}
+        {val, theta} -> {val, theta}
+      end)
+
+    # reversal not strictly necessary, but nice to keep it ordered
+    {outer_rule, inner_rule}
   end
 
   defp ticks_and_labels(val) do
     cond do
-      Integer.mod(val, 5) == 0 -> %{label: Integer.to_string(val), stroke_width: "1.0"}
-      true -> %{label: nil, stroke_width: "0.5"}
+      Integer.mod(val, 5) == 0 ->
+        %{label: Integer.to_string(val), tick_length: 10, stroke_width: "1.0"}
+
+      true ->
+        %{label: nil, tick_length: 10, stroke_width: "0.5"}
     end
   end
 end
