b1e93ccaaf7b67e6b7efe8b5189dc64e65ee2ab5
Ben Swift <ben@benswift.me>
2024-07-08 10:21:10 +1000

    update az sliders to have labels "inline"

--- lib/perceptron_apparatus/rings/azimuthal_sliders.ex
+++ lib/perceptron_apparatus/rings/azimuthal_sliders.ex
@@ -22,7 +22,7 @@ defmodule PerceptronApparatus.Rings.AzimuthalSliders do
     # use default values when it makes sense
     range = Keyword.get(opts, :range, PerceptronApparatus.Utils.drange(0, 1, 0.1))
 
-    %__MODULE__{width: 26.0, range: range, shape: shape}
+    %__MODULE__{width: 20.0, range: range, shape: shape}
   end
 
   # range is a list of Decimals, but for calculation purposes we convert them to floats early
@@ -33,55 +33,83 @@ defmodule PerceptronApparatus.Rings.AzimuthalSliders do
     range_max = List.last(range) |> D.to_float()
     dynamic_range = range_max - range_min
 
+    # for creating "gaps" at the beginning and end of the [theta_offset, theta_offset + theta_sweep]
+    # range (where the labels will go)
+    az_padding = 1500 / radius
+
     labels =
       range
       |> Enum.map(fn val ->
-        theta = theta_sweep * (D.to_float(val) - range_min) / dynamic_range
-        %{label: label, stroke_width: stroke_width} = ticks_and_labels(val)
+        theta =
+          az_padding +
+            (theta_sweep - 2 * az_padding) * (D.to_float(val) - range_min) / dynamic_range
+
+        %{stroke_width: stroke_width} = ticks_and_labels(val)
 
         """
         <g transform="rotate(#{-theta})"  transform-origin="0 0">
-          <line class="top etch" x1="0" y1="#{radius + slider_hwidth}" x2="0" y2="#{radius + slider_hwidth * 3}" stroke-width="#{stroke_width}" />
-          <text class="top etch" x="0" y="#{radius + slider_hwidth * 6}"
-                style="font-size: 12px;" fill="black" stroke="none" stroke-width="#{stroke_width}"
-                text-anchor="middle" dominant-baseline="middle"
-                >#{label}</text>
+          <line class="top etch" x1="0" x2="0" y1="#{radius - slider_hwidth * 3}" y2="#{radius + slider_hwidth * 3}" stroke-width="#{stroke_width}" />
         </g>
         """
       end)
+      |> List.insert_at(
+        0,
+        """
+        <g transform="rotate(#{-(theta_offset + 2 / 3 * az_padding)})"  transform-origin="0 0">
+          <text class="top etch" x="0" y="#{radius}"
+                style="font-size: 12px;" fill="black" stroke="none" stroke-width="#{range |> List.first() |> ticks_and_labels() |> Map.get(:stroke_width)}"
+                text-anchor="middle" dominant-baseline="middle"
+                >#{range |> List.first() |> ticks_and_labels() |> Map.get(:label)}</text>
+        </g>
+        """
+      )
+      |> List.insert_at(
+        -1,
+        """
+        <g transform="rotate(#{-(theta_offset + theta_sweep - 2 / 3 * az_padding)})"  transform-origin="0 0">
+          <text class="top etch" x="0" y="#{radius}"
+                style="font-size: 12px;" fill="black" stroke="none" stroke-width="#{range |> List.last() |> ticks_and_labels() |> Map.get(:stroke_width)}"
+                text-anchor="middle" dominant-baseline="middle"
+                >#{range |> List.last() |> ticks_and_labels() |> Map.get(:label)}</text>
+        </g>
+        """
+      )
       |> Enum.join()
 
-    x1 = 0
-    y1 = radius - slider_hwidth
-    x2 = 0
-    y2 = radius + slider_hwidth
-    x3 = (radius + slider_hwidth) * Math.sin(Math.deg2rad(theta_sweep))
-    y3 = (radius + slider_hwidth) * Math.cos(Math.deg2rad(theta_sweep))
-    x4 = (radius - slider_hwidth) * Math.sin(Math.deg2rad(theta_sweep))
-    y4 = (radius - slider_hwidth) * Math.cos(Math.deg2rad(theta_sweep))
+    x1 = (radius + slider_hwidth) * Math.sin(Math.deg2rad(az_padding))
+    y1 = (radius - slider_hwidth) * Math.cos(Math.deg2rad(az_padding))
+    x2 = (radius - slider_hwidth) * Math.sin(Math.deg2rad(az_padding))
+    y2 = (radius + slider_hwidth) * Math.cos(Math.deg2rad(az_padding))
+    x3 = (radius + slider_hwidth) * Math.sin(Math.deg2rad(theta_sweep - az_padding))
+    y3 = (radius + slider_hwidth) * Math.cos(Math.deg2rad(theta_sweep - az_padding))
+    x4 = (radius - slider_hwidth) * Math.sin(Math.deg2rad(theta_sweep - az_padding))
+    y4 = (radius - slider_hwidth) * Math.cos(Math.deg2rad(theta_sweep - az_padding))
 
     """
     <g transform="rotate(#{-theta_offset})"  transform-origin="0 0">
+    #{labels}
      <path
       class="top full"
+      style="fill: white;"
       d="M #{x1} #{y1}
         A #{slider_hwidth} #{slider_hwidth} 0 0 0 #{x2} #{y2}
         A #{radius + slider_hwidth} #{radius + slider_hwidth} 0 0 0 #{x3} #{y3}
         A #{slider_hwidth} #{slider_hwidth} 0 0 0 #{x4} #{y4}
         A #{radius - slider_hwidth} #{radius - slider_hwidth} 0 0 1 #{x1} #{y1}"
       />
-      #{labels}
       </g>
     """
   end
 
   def render(radius, sliders, range) do
+    theta_sweep = 360 / sliders
+
     0..(sliders - 1)
-    |> Enum.map(fn val ->
+    |> Enum.map(fn i ->
       render_slider(
         radius,
-        360 / sliders - 1800 / radius,
-        360 * val / sliders,
+        theta_sweep,
+        theta_sweep * i,
         range
       )
     end)
@@ -112,6 +140,6 @@ defimpl PerceptronApparatus.Renderable, for: PerceptronApparatus.Rings.Azimuthal
   def render(ring) do
     %{range: range, shape: {sliders}, context: {radius, _layer_index}} = ring
 
-    AzimuthalSliders.render(radius - 22, sliders, range)
+    AzimuthalSliders.render(radius - 10, sliders, range)
   end
 end
