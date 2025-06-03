d2aef1162002660a5ff4c91ca210b081ed106fed
Ben Swift <ben@benswift.me>
2025-06-03 14:52:34 +1000

    remove some boilerplate comments

--- mix.exs
+++ mix.exs
@@ -12,14 +12,12 @@ defmodule PerceptronApparatus.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:axon, "~> 0.7"},
@@ -30,8 +28,6 @@ defmodule PerceptronApparatus.MixProject do
       {:ash, "~> 3.0"},
       {:igniter, "~> 0.6", only: [:dev, :test]},
       {:lazy_html, "~> 0.1.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
